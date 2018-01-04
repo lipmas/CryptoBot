02e64453a99095a1aa3ec2762658f45be7685fef
Sebby <lipmas93@gmail.com>
2018-01-04 08:37:48 -0500

    rename to secrets.cfg

--- Constants/Config.cs
+++ Constants/Config.cs
@@ -6,7 +6,7 @@ namespace CryptoBot.Constants {
     class PoloniexConfig {
         public const string poloniexApiPublicBaseUrl = "https://poloniex.com/public";
         public const string poloniexApiPrivateBaseUrl = "https://poloniex.com/tradingApi";
-        public const string secretsFile = "my_secrets.cfg";
+        public const string secretsFile = "secrets.cfg";
         public const string nonceFile = "last_nonce";
     }
 }
